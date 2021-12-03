f24ffca2302170e9b2f884f120552bff6a0a147c
Fathi Rabbani <fathirabbani2707@gmail.com>
2021-12-03 23:47:15 +0700

    add weDocs & Passster Plugin

---
+++ astory/wp-content/plugins/content-protector/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bc70b945db0215d848732d3dc7702205b556f37c',
+    'name' => 'patrickposner/passster',
+  ),
+  'versions' => 
+  array (
+    'patrickposner/passster' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bc70b945db0215d848732d3dc7702205b556f37c',
+    ),
+  ),
+);
